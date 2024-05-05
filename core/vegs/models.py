4aebe67971aead78d10a85ef70c28f0e66827a32
Rahmath <Rahmathulla42627gmail.com>
2024-05-05 15:09:37 +0530

    added btn

--- core/vegs/models.py
+++ core/vegs/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Recepie(models.Model):
     recepie_name = models.CharField(max_length=100)
-    recepie_description = models.TextField()
+    text = models.TextField()
     receipe_image = models.ImageField(upload_to="recepie")
--- core/vegs/views.py
+++ core/vegs/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.http import HttpResponse
 
 # Create your views here.
 def receipes(request):
-    return render(request, 'receipes.html')
+    if request.method=="POST":
+     data=request.POST
+     receipe_image = request.FILES.get('receipe_image')
+     recepie_name = data.get('recepie_name')
+     text = data.get('text')
+
+     Recepie.objects.create(
+         receipe_image = receipe_image,
+         recepie_name = recepie_name,
+         text = text,
+     )
+
+     return redirect('/receipes')
+    
+    queryset= Recepie.objects.all()
+    context = {'receipes': queryset}    
+    return render(request, 'receipes.html',context)
+
+def delete_receipe(request,id):
+   queryset = Recepie.objects.get(id= id)
+   queryset.delete()
+   return redirect('/receipes')
+  
