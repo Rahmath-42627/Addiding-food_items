0cbfedbb072318ccf10271a0b48f4f5fd0c5974e
Rahmath <Rahmathulla42627gmail.com>
2024-05-14 22:56:31 +0530

    updated login,logout

--- core/vegs/views.py
+++ core/vegs/views.py
@@ -3,8 +3,11 @@ from .models import *
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='/login/')
 def receipes(request):
     if request.method =="POST":
 
